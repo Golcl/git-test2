b16e31c8f5c852448ffb15cfd5f47c116228fef5
lcl <2391278656@qq.com>
2021-07-19 19:13:40 +0800

    03 commit

--- src/main/java/com/lcl/test01.java
+++ src/main/java/com/lcl/test01.java
@@ -12,5 +12,6 @@ public class test01 {
     public static void main(String[] args) {
         System.out.println("hello Git!");
         System.out.println("hello Git2!");
+        System.out.println("hello Git3!");
     }
 }
