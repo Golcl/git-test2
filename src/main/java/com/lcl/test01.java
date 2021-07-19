abe3c2fdad13b731f490a97a13a688ab432585c2
lcl <2391278656@qq.com>
2021-07-19 18:57:54 +0800

    first commit

---
+++ src/main/java/com/lcl/test01.java
@@ -0,0 +1,15 @@
+package com.lcl;
+
+import sun.applet.Main;
+
+/**
+ * @Auther:吕朝林
+ * @Date: 2021/7/19-07-19-18:53
+ * @Description: com.lcl
+ * @version: 1.0
+ */
+public class test01 {
+    public static void main(String[] args) {
+        System.out.println("hello Git!");
+    }
+}
